2cad38fe6bef8891bedfd03bbcf8049f9de28538
wicked777 <wicky.akloe@gmail.com>
2019-08-06 21:37:38 +0200

    Added test between function

--- byotest.py
+++ byotest.py
@@ -25,6 +25,10 @@ def test_not_in(collection, item):
         assert item not in collection, "{0} does contain {1}".format(collection, item)
 
 
+def test_between(lower_limit, upper_limit, actual):
+    assert lower_limit <= actual <= upper_limit, "The actual no:{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
+
+
 # Tests to fail
 # test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
 # test_not_equal(2, 2)
@@ -32,4 +36,5 @@ def test_not_in(collection, item):
 # test_is_in((2, 3, 4, 5), 1)
 # test_is_in({"red": 1, "blue": 2}, 3)
 # test_not_in({"red": 1, "blue": 2}, "red")
-test_not_in((2, 3, 4, 5), 2)
+# test_not_in((2, 3, 4, 5), 2)
+# test_between(100, 200, 201)
