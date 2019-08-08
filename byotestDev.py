070df577b82320b9b1d16d90ce2a68f92063a8b6
wicked777 <wicky.akloe@gmail.com>
2019-08-08 17:27:43 +0200

    Rename off original file to byotestDev.py and made new version for reuse

--- byotestDev.py
+++ byotestDev.py
@@ -1,3 +1,7 @@
+def number_of_evens(numbers):
+    return 0
+
+
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
     print("test_are_equal - Passed tests")
